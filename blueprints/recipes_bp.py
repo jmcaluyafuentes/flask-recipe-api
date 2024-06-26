3fc6aa0663e5294d0f37c9ea4bbfd7930768537b
John Fuentes <jmcaluyafuentes@gmail.com>
2024-06-26 21:19:01 +1000

    Added route to update a recipe

--- blueprints/recipes_bp.py
+++ blueprints/recipes_bp.py
@@ -4,6 +4,7 @@ This module is a blueprint for routes to manage recipe records.
 
 from datetime import date
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required
 from init import db
 from models.recipe import Recipe, RecipeSchema
 
@@ -38,6 +39,7 @@ def one_recipe(recipe_id):
     return RecipeSchema().dump(recipe)
 
 @recipes_bp.route("/", methods=["POST"])
+@jwt_required()
 def create_recipe():
     """
     This function handles POST requests to create a new recipe. It expects the request 
@@ -66,4 +68,43 @@ def create_recipe():
 
     db.session.add(recipe)
     db.session.commit()
+    return RecipeSchema().dump(recipe), 201
+
+@recipes_bp.route("/<int:recipe_id>", methods=["PUT", "PATCH"])
+def update_recipe(recipe_id):
+    """
+    Endpoint to update an existing recipe.
+
+    This function handles PUT and PATCH requests to update an existing recipe by its ID. 
+    It expects the request body to contain one or more fields of the recipe that need to be updated, 
+    including title, description, is_public flag, and preparation time. Any fields not provided 
+    in the request will remain unchanged.
+
+    Path Parameters:
+        recipe_id (int): The ID of the recipe to be updated.
+
+    Request Body (JSON):
+        title (str, optional): The new title of the recipe.
+        description (str, optional): The new description of the recipe.
+        is_public (bool, optional): The new visibility status of the recipe.
+        preparation_time (int, optional): The new preparation time in minutes.
+
+    Returns:
+        tuple: A tuple containing the serialized updated recipe data and an HTTP status code.
+            - dict: The serialized updated recipe data.
+            - int: HTTP status code 200 indicating that the recipe was successfully updated.
+
+    Raises:
+        KeyError: If the input data does not conform to the expected schema.
+        NotFound: If the recipe with the given ID does not exist.
+    """
+    recipe = db.get_or_404(Recipe, recipe_id)
+    recipe_info = RecipeSchema(only=['title', 'description', 'is_public', 'preparation_time']).load(request.json, unknown='exclude')
+
+    recipe.title = recipe_info.get('title', recipe.title)
+    recipe.description = recipe_info.get('description', recipe.description)
+    recipe.is_public = recipe_info.get('is_public', recipe.is_public)
+    recipe.preparation_time = recipe_info.get('preparation_time', recipe.preparation_time)
+    db.session.commit()
     return RecipeSchema().dump(recipe)
+
